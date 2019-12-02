fa0c21a6a77772cfc052505b7e800624934e3cb4
jismongeorge95 <57097645+jismongeorge95@users.noreply.github.com>
2019-12-02 10:11:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+var express=require('express');
+const app=express();
+app.get("/",function(req,res)
+{
+    res.send("welcome")
+});
+const port=process,env,PORT || 3000
+app.listen(3000,function(){
+    Console.log("port 3000 is using");
+});
